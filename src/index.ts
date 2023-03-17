d5fcdfc16ca563cc095ddf9a886fc319f459df2d
Kauê Sabino Villaça de Almeida <kauevillaca2018@gmail.com>
2023-03-16 23:37:29 -0300

    feat: fastfy server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('oi')
+import fastify, { FastifyRequest, FastifyReply } from 'fastify';
+
+const server = fastify();
+
+server.get('/members', async (request: FastifyRequest, reply: FastifyReply) => {
+  return { hello: 'world' };
+});
+
+server.listen(3000, (err) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log('Server started on port 3000');
+});
